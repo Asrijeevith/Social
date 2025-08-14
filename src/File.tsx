d9d86a8ce3d06869c1434e8ff49c7f5c5ba752b0
nitishbytaru <bndnitish24@gmail.com>
2025-08-14 15:28:10 +0530

    update

---
+++ src/File.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function File() {
+  return (
+    <View>
+      <Text>File</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
